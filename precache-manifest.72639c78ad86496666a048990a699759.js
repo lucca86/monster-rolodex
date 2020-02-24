2c05a38887a8a3bb90505dc14bc65fbe71172def
Lucca <lucca86@gmail.com>
2020-02-24 10:47:19 -0300

    Updates

---
+++ precache-manifest.72639c78ad86496666a048990a699759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f786b0d5e10388f7eb7b5148ae5ae84f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1cf1e4d376caf4a84f4c",
+    "url": "/monster-rolodex/static/css/main.24de82d5.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf1e4d376caf4a84f4c",
+    "url": "/monster-rolodex/static/js/main.4985db33.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
